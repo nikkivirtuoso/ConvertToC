2530aea85b413c3b06821ee6c00c00bb01e8113f
Nishant Kumar <nishantkumar@Nishants-MacBook-Air.local>
2024-02-28 00:18:05 +0530

    last

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,23 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   try {
-    // Make HTTP GET request
     String url = "https://reqres.in/api/users/2";
     var response = await http.get(Uri.parse(url));
 
-    // Check if the request was successful (status code 200)
     if (response.statusCode == 200) {
-      // Generate C code to print the response
       String cCode = generateCCodeSnippet(url);
       print('Generated C code:');
       print(cCode);
     } else {
-      // If the request was not successful, print the status code
       print('Request failed with status: ${response.statusCode}');
     }
   } catch (e) {
-    // Handle any errors that occur during the request
     print('Error: $e');
   }
 }
 
 String generateCCodeSnippet(String apiUrl) {
-  // Escape double quotes in the API URL
   String escapedUrl = apiUrl;
-
-  // Generate C code snippet using libcurl
   return '''
 #include <stdio.h>
 #include <curl/curl.h>
